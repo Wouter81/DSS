b6678bb3bc9a5cfc8cdb83804aa7d5ced3bdbba4
dutchsportstreams <patrick@paradiseradio.org>
2016-09-15 19:29:23 +0200

    update

--- plugin.video.dss/default.py
+++ plugin.video.dss/default.py
@@ -212,48 +212,6 @@ def getutfoffset():
 
 
 
-def AddBvls(url=None):
-    i=0
-    progress = xbmcgui.DialogProgress()
-    progress.create('Progress', 'Scanning Bvls2016.sc')
-    sourceSite = 'http://www.bvls2016.sc/'
-    xmlurl = 'http://dutchsportstreams.com/xml/ds.xml'
-    req = urllib2.Request(xmlurl,None)
-    response = urllib2.urlopen(req)
-    xml = response.read()
-    response.close()
-    root = ET.fromstring(xml)
-    items = root.findall('item')
-    for item in items:
-        cname = item.find('title').text
-        lnk = item.find('url').text
-        print cname
-        print lnk
-        try :
-            frameHtml = getPage(lnk,'http://www.bvls2016.sc/', getUserAgent())
-            b64coded = getBaseEncodedString(frameHtml)
-            streamUrl = getStreamUrl(b64coded)
-            i+=1
-            progress.update( 15+ (i*15), "BVLS2016.SC", "Finding links.. stream%d"%i, "" )
-        except :
-            streamUrl = ''
-        if (getResponse(streamUrl)) :
-            color = 'green'
-        else :
-            #streamUrl = ''
-            color = 'red'
-        if streamUrl.startswith('rtmp'):
-            
-            color = 'blue'
-        if streamUrl[-4:] == '.flv' :
-            streamUrl=VeetleId(streamUrl)
-        #else :
-            #print('M3U')
-        #if 'youtube' in streamUrl:
-            #streamUrl=streamUrl.replace('https://www.youtube.com/watch?v=', 'plugin://plugin.video.youtube/play/?video_id=').strip()
-        addDir('[COLOR '+color+']Bvls2016.sc - '+cname.capitalize()+'[/COLOR]',streamUrl,14,icon ,  FANART,'','','','',isItFolder=False)
-        
-    progress.close()
 
 def getResponse(url):
     try:
@@ -283,127 +241,6 @@ def AddSports365Channels(url=None):
             #ok = dialog.ok('XBMC', 'No Links, so updated files dyamically, try again, just in case!')           
             #print 'Updated files'
 
-def CatClubchannels():
-    import plugintools
-    plugintools.add_item(title="AFC Ajax",url="plugin://plugin.video.youtube/user/ajax/",thumbnail='https://yt3.ggpht.com/-jqrIEltgE1U/AAAAAAAAAAI/AAAAAAAAAAA/AhkDhss9X4w/s100-c-k-no/photo.jpg',folder=True )
-    plugintools.add_item(title="Feyenoord",url="plugin://plugin.video.youtube/user/FeyenoordRotterdamTV/",thumbnail="https://yt3.ggpht.com/-sfO41QeVlw4/AAAAAAAAAAI/AAAAAAAAAAA/hDDl3jwRL1k/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="PSV",url="plugin://plugin.video.youtube/user/psveindhoven/",thumbnail="https://i.ytimg.com/i/_2ynsXrRrKP8zYrU7Hc06A/mq1.jpg?v=53302bc9",folder=True )
-    plugintools.add_item(title="Pec Zwolle",url="plugin://plugin.video.youtube/user/peczwolletv/",thumbnail="https://yt3.ggpht.com/-ShDWQyu69vk/AAAAAAAAAAI/AAAAAAAAAAA/klALDUBVkFs/s100-c-k-no-rj-c0xffffff/photo.jpg",folder=True )
-    plugintools.add_item(title="Heracles Almelo",url="plugin://plugin.video.youtube/user/HeraclesAlmeloTV/",thumbnail="https://yt3.ggpht.com/-4syNFL3i7WA/AAAAAAAAAAI/AAAAAAAAAAA/9uxdYzc6z-8/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="Roda JC Kerkrade",url="plugin://plugin.video.youtube/user/RodaJCKerkradeTV/",thumbnail="https://yt3.ggpht.com/-LhF3zdjpng4/AAAAAAAAAAI/AAAAAAAAAAA/peTKp7TXFYQ/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="ADO Den Haag",url="plugin://plugin.video.youtube/user/ADODenHaagTV/",thumbnail="https://yt3.ggpht.com/-6RvgIEV9WhI/AAAAAAAAAAI/AAAAAAAAAAA/eYEVcEyJTHU/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="Vitesse",url="plugin://plugin.video.youtube/user/VitesseTV/",thumbnail="https://yt3.ggpht.com/-ewXQBcFk6ZE/AAAAAAAAAAI/AAAAAAAAAAA/1_nJq7G_iqo/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="FC Groningen",url="plugin://plugin.video.youtube/user/FCGroningenTV/",thumbnail="https://yt3.ggpht.com/-vatxowB6e2o/AAAAAAAAAAI/AAAAAAAAAAA/PySf0KkU7ZM/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="N.E.C.",url="plugin://plugin.video.youtube/user/NECTVkanaal/",thumbnail="https://yt3.ggpht.com/-KpZ8RDTeTfQ/AAAAAAAAAAI/AAAAAAAAAAA/pVeT-DTm5kk/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="FC Utrecht",url="plugin://plugin.video.youtube/user/fcutrecht/",thumbnail="https://yt3.ggpht.com/-3RaZ5yClYxg/AAAAAAAAAAI/AAAAAAAAAAA/R4o-6Jk6x8M/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="AZ",url="plugin://plugin.video.youtube/user/AZTV/",thumbnail="https://yt3.ggpht.com/-yyhnHNLCPp8/AAAAAAAAAAI/AAAAAAAAAAA/xlm_pFvukqI/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="Willem II",url="plugin://plugin.video.youtube/user/WillemII/",thumbnail="https://yt3.ggpht.com/-738-M5uYXlg/AAAAAAAAAAI/AAAAAAAAAAA/0k0wAIqPq30/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="Excelsior",url="plugin://plugin.video.youtube/user/sbvexcelsior/",thumbnail="https://yt3.ggpht.com/-32h9c3Rz-ao/AAAAAAAAAAI/AAAAAAAAAAA/1ANEn8xjySU/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="FC Twente",url="plugin://plugin.video.youtube/user/FCTwenteTV/",thumbnail="https://yt3.ggpht.com/-TKHbRZL1kb4/AAAAAAAAAAI/AAAAAAAAAAA/cyqeyk8i8RM/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="SC Cambuur",url="plugin://plugin.video.youtube/user/SCCambuurTV/",thumbnail="https://yt3.ggpht.com/-Fb4cKM1YAto/AAAAAAAAAAI/AAAAAAAAAAA/uBPlhE7gn7E/s100-c-k-no/photo.jpg",folder=True )
-    plugintools.add_item(title="De Graafschap",url="plugin://plugin.video.youtube/user/degraafschapvideo/",thumbnail="https://yt3.ggpht.com/--eXA14VMeVU/AAAAAAAAAAI/AAAAAAAAAAA/ju7WtvhOsg0/s100-c-k-no/photo.jpg",folder=True )
-    #plugintools.add_item(title="",url="plugin://plugin.video.youtube/user//",thumbnail="",folder=True )
-
-
-
-
-def Catfeed2all():
-        addDir('Football' ,'football.html',18,icon ,  FANART,'','','','')
-        addDir('AM. Football' ,'american-football.html',18,icon ,  FANART,'','','','')
-        addDir('Basketball' ,'basketball.html',18,icon ,  FANART,'','','','')
-        addDir('Boxing/WWE/UFC' ,'boxing-wwe-ufc.html',18,icon ,  FANART,'','','','')
-        addDir('Rugby' ,'rugby.html',18,icon ,  FANART,'','','','')
-        addDir('Ice Hockey' ,'ice-hockey.html',18,icon ,  FANART,'','','','')
-        addDir('Tennis' ,'tennis.html',18,icon ,  FANART,'','','','')
-        addDir('Motorsport' ,'motosport.html',18,icon ,  FANART,'','','','')
-        addDir('Golf' ,'golf.html',18,icon ,  FANART,'','','','')
-        addDir('Baseball' ,'baseball.html',18,icon ,  FANART,'','','','')
-        addDir('Darts' ,'darts.html',18,icon ,  FANART,'','','','')
-        addDir('Snooker' ,'snooker.html',18,icon ,  FANART,'','','','')
-        addDir('Handball' ,'handball.html',18,icon ,  FANART,'','','','')
-        addDir('Cricket' ,'cricket.html',18,icon ,  FANART,'','','','')
-        addDir('Aussie Rules' ,'aussie-rules.html',18,icon ,  FANART,'','','','')
-        addDir('Others' ,'others.html',18,icon ,  FANART,'','','','')
-	return
-
-
-
-def Addthefeed2all(url):
-    try:
-        pagecontent=getPage('http://www.feed2allnow.eu/type/'+url,'http://www.feed2allnow.eu/',getUserAgent())
-        match1=re.compile('<span>\s*([^<]+)</span>\s*([^<]+) </a> </h3>.+?\s*.+?href=\'(.*?)\'',re.DOTALL).findall(pagecontent)
-        for dsstime,dssgame,dsslink in match1:
-            dsslink = 'http://www.feed2allnow.eu' + dsslink
-            addDir('[B][COLOR yellow3]'+ dssgame+ '[/COLOR][/B]',dsslink,3,icon ,  FANART,'','','','',isItFolder=False)
-    except:
-        pass
-    try:
-        pagecontent=getPage('http://www.feed2allnow.eu/type/'+url,'http://www.feed2allnow.eu/',getUserAgent())
-        match1=re.compile('<span class="matchtime">([^<]+)</span> </span>\s*([^<]+)</a> </h3>.+?\s*.+?href=\'([^<]+)\'',re.DOTALL).findall(pagecontent)
-        for dsstime,dssgame,dsslink in match1:
-            try:
-                dt = datetime.strptime(dsstime, "%H:%M")
-                dsstime = str(dt.hour+1).rjust(2,'0') + ':' + str(dt.minute).rjust(2,'0')
-            except :
-                pass        
-            dsslink = 'http://www.feed2allnow.eu' + dsslink
-            addDir('[B][COLOR yellow3]''('+dsstime+') '+ dssgame+ '[/COLOR][/B]',dsslink,3,icon ,  FANART,'','','','',isItFolder=False)
-    except:
-        pass
-    
-
-
-def Addwiz(url=None):
-    pagecontent = getPage('http://www.wiz1.net/lag10_home.php','http://www.wiz1.net/',getUserAgent())
-    match = re.compile(r'(\d{2}:\d{2}) <font color="#5185C9"><b>([^<]+)</b></font> ([^<]+)<a href="([^"]+)"', re.DOTALL | re.IGNORECASE).findall(pagecontent)
-    for dsstime, sport, game, dsslink in match:
-        try:
-            dt = datetime.strptime(dsstime, "%H:%M")
-            dsstime = str(dt.hour+0).rjust(2,'0') + ':' + str(dt.minute).rjust(2,'0')
-        except :
-            pass
-        #if 'Eredivisie' in sport or 'Belgium' in sport:
-            #addDir('[B][COLOR yellow3]''('+dsstime+') '+ game+ '[/COLOR][/B]',dsslink,21,icon ,  FANART,'','','','',isItFolder=False)
-        #if 'Netherlands' in game or 'Belgium' in game:
-        addDir('[B][COLOR yellow3]''('+sport+') '+'('+dsstime+') '+ game+ '[/COLOR][/B]',dsslink,3,icon ,  FANART,'','','','',isItFolder=False)
-        
-
-
-
-def Addgoatd(url=None):
-    dssport=''
-    goatpage = getPage('http://goatd.net/','http://goatd.net/',getUserAgent())
-    match = re.compile(r'<b>ET</b></td>\s+<td[^<]+><img src="([^"]+)".*?href="([^"]+)"[^>]+>([^<]+)<.*?<b>([^<]+)<.*?<b>([^<]+)<', re.DOTALL | re.IGNORECASE).findall(goatpage)
-    for img, dsslink, dssgame, dsstime, dsszone in match:
-        try:
-            sport = re.compile('http://.*?/.*?/.*?/.*?/.*?/.*?/(.*?).gif', re.DOTALL | re.IGNORECASE).findall(img)
-            if sport > 0 :
-                for dssport in sport:
-                    dssport = re.sub("[^A-Za-z]", "", dssport) 
-        except:
-            pass
-        dsslink = 'http://goatd.net/' + dsslink
-        addDir('[B][COLOR yellow3]''('+dssport+') '+'('+dsstime+') '+dsszone+' '+ dssgame+ '[/COLOR][/B]',dsslink,3,icon ,  FANART,'','','','',isItFolder=False)
-        
-
-
-
-
-
-def AddCricFree(url):
-    pat='<li.*?><a href="(.*?)".*?channels-icon (.*?)"'
-    res=get_Url("http://cricfree.sx/")
-    channels=re.findall(pat,res)
-    
-    pat='<li><a href="(.*?)".*?\<span class="chclass3"\>(.*?)<'
-    channels+=re.findall(pat,res)    
-#    channels=sorted(channels,key=lambda s: s[1].lower()   )
-    channels=sorted_nicely(channels)
-    for u,n in channels:
-        addDir(n.capitalize(),u,17,icon ,  FANART,'','','','',isItFolder=False)
-
-
-
 
 
 def playSports365(url):
@@ -413,7 +250,7 @@ def playSports365(url):
     if urlToPlay and len(urlToPlay)>0:
         listitem = xbmcgui.ListItem( label = str(name), iconImage = "DefaultVideo.png", thumbnailImage = xbmc.getInfoImage( "ListItem.Thumb" ) )
     #    print   "playing stream name: " + str(name)
-        xbmc.executebuiltin('XBMC.Notification(Visit sport365.live , And keep Streams Alive !! ,10000,'+icon+')')
+        #xbmc.executebuiltin('XBMC.Notification(Visit sport365.live , And keep Streams Alive !! ,10000,'+icon+')')
         xbmc.Player(  ).play( urlToPlay, listitem)  
     #else:
        #if RefreshResources([('live365.py','https://')]):
@@ -423,72 +260,7 @@ def playSports365(url):
     return	 
 
 
-def PlayCricFree(url):
-    progress = xbmcgui.DialogProgress()
-    progress.create('Progress', 'Fetching Streaming Info')
-    progress.update( 10, "", "Finding links..", "" )
 
-    res=getUrl(url)
-    patt='<iframe frameborder="0" marginheight="0".*?src="(.*?)" id="iframe"'
-    url2=re.findall(patt,res)[0]
-    referer=[('Referer',url)]
-    res=get_Url(url2,headers=referer)
-    urlToPlay=None
-    supported=False
-    if 'theactionlive.com/' in res:
-        supported=True
-        progress.update( 30, "", "Finding links..stage2", "" )
-        patt="id='(.*?)'.*?width='(.*)'.*?height='(.*?)'"
-        gid,wd,ht=re.findall(patt,res)[0]
-        referer=[('Referer',url2)]
-        url3='http://theactionlive.com/livegamecr2.php?id=%s&width=%s&height=%s&stretching='%(gid,wd,ht)
-        res=get_Url(url3,headers=referer)    
-        if 'biggestplayer.me' in res:
-            progress.update( 50, "", "Finding links..stage3", "" )
-            patt="id='(.*?)'.*?width='(.*)'.*?height='(.*?)'"
-            gid,wd,ht=re.findall(patt,res)[0]
-            referer=[('Referer',url3)]
-            
-            patt="src='(.*?)'"
-            jsUrl=re.findall(patt,res)[0]
-            jsData=getUrl(jsUrl)
-            patt="\.me\/(.*?)\?"
-            phpURL=re.findall(patt,jsData)[0]
-            url4='http://biggestplayer.me/%s?id=%s&width=%s&height=%s'%(phpURL,gid,wd,ht)
-            progress.update( 80, "", "Finding links..last stage", "" )
-            res=get_Url(url4,headers=referer)    
-            patt='file: "(.*?)"'
-            urlToPlay=re.findall(patt,res)[0];
-            referer=[('Referer',url4)]
-            urlToPlay+='|Referer='+url4
-    if 'www.reytv.co' in res:
-        supported=True
-        progress.update( 30, "", "Finding links..stage2", "" )
-        patt="fid='(.*?)'.*?v_width=(.*?);.*?v_height=(.*?);"
-        gid,wd,ht=re.findall(patt,res)[0]
-        referer=[('Referer',url2)]
-        url3='http://reytv.co/embedo.php?live=%s&width=%s&height=%s'%(gid,wd,ht)
-        progress.update( 50, "", "Finding links..stage3", "" )
-        res=get_Url(url3,headers=referer)
-        
-        patt='file: "(.*?)"'
-        rtmp=re.findall(patt,res)[0]
-        patt='securetoken: "(.*?)"'
-        token=re.findall(patt,res)[0]           
-        urlToPlay=rtmp + ' token=' + token + ' pageUrl='+url3+ ' swfUrl=http://p.jwpcdn.com/6/12/jwplayer.flash.swf'+' timeout=20'
-    
-    if urlToPlay and len(urlToPlay)>0:
-        playlist = xbmc.PlayList(1)
-        playlist.clear()
-        listitem = xbmcgui.ListItem( label = str(name), iconImage = "DefaultVideo.png", thumbnailImage = xbmc.getInfoImage( "ListItem.Thumb" ) )
-        playlist.add(urlToPlay,listitem)
-        xbmcPlayer = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
-        xbmcPlayer.play(playlist) 
-    else:
-        dialog = xbmcgui.Dialog()
-        if not supported:
-            ok = dialog.ok('Not Supported','This channel is not supported yet')
-   
     
    
 
@@ -521,11 +293,7 @@ def convert(s):
         
 
 
-def playBvls(name,url):
-    listitem = xbmcgui.ListItem( label = str(name), iconImage = "DefaultVideo.png", thumbnailImage = xbmc.getInfoImage( "ListItem.Thumb" ) )
-#    print "playing stream name: " + str(name)
-    xbmc.executebuiltin('XBMC.Notification(Visit www.bvls2016.sc , And keep Streams Alive !! ,10000,'+icon+')')
-    xbmc.Player( xbmc.PLAYER_CORE_AUTO ).play( url, listitem)
+
 	
 
 def playVeetle(url):
@@ -648,8 +416,7 @@ print 	mode,url,linkType
 
 try:
 	if mode==None or url==None or len(url)<1:
-		print "InAddTypes"
-		Addtypes()
+		AddSports365Channels('Sport365')
 
 
         elif mode==2:
@@ -664,7 +431,7 @@ try:
     
 	elif mode==12 :
 		print "Play url is 12"+url
-		AddSports365Channels(url)
+		AddSports365Channels('Sport365')
 
 	elif mode==13 :
 		print "Play url is 12"+url
