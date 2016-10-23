5f24fe4043215ebb6eb7d56cd8fd9ed5c6487da8
dutchsportstreams <patrick@paradiseradio.org>
2016-10-23 09:35:32 +0200

    update

--- plugin.video.vi/default.py
+++ plugin.video.vi/default.py
@@ -68,10 +68,14 @@ def GetHTML(url):
 
 
 def MainDir():
-    addDir('Day' ,vi_day,1,icon)
-    addDir('Week' ,vi_week,1,icon)
-    addDir('month' ,vi_month,1,icon)
-    addDir('Classics' ,vi_classics,1,icon)
+    addDir('[B][COLOR yellow]Day[/COLOR][/B]' ,'&type=day',1,icon,page=1,channel='top',fanart=fanart)
+    addDir('[B][COLOR yellow]Week[/COLOR][/B]' ,'&type=week',1,icon,page=1,channel='top',fanart=fanart)
+    addDir('[B][COLOR yellow]month[/COLOR][/B]' ,'&type=month',1,icon,page=1,channel='top',fanart=fanart)
+    addDir('[B][COLOR yellow]Classics[/COLOR][/B]' ,'&type=classics',1,icon,page=1,channel='top',fanart=fanart)
+    addDir('[B][COLOR yellow]Zoeken[/COLOR][/B]' ,'&searchTerm=',2,icon,page=1,channel='search',fanart=fanart)
+    addDir('[B][COLOR orange]---- Uitgelicht ----[/COLOR][/B]' ,'classics',0,icon,page=1,channel='top',fanart=fanart)
+    vi_read('&type=',page=1,channel='home')
+
 
 
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
@@ -82,27 +86,53 @@ def MainDir():
         
 
 
-def vi_read(url):
-    livejson = GetHTML(url)
-    livejson = json.loads(livejson, encoding='utf-8')
-    for items in livejson:
-        name = str(items)
-    for items in livejson[name]["items"]:
-        title = items["title"]
-        title = title.encode('utf-8')
-        title = removeNonAscii(title)
-        timeDisplay = items["timeDisplay"]
-        print timeDisplay
-        print title
-        videoUuid = items["videoUuid"]
-        print videoUuid
-        image = items["image"]
-        image = 'http://rtl.lwcdn.nl/imageScaled/?site=voetbalinside&file='+image+'&w=500.000000&h=500.000000&cropped=1'
-        print image
-        url = get_video('http://www.rtl.nl/system/s4m/vfd/version=2/d=iphone/fmt=adaptive/fun=abstract/uuid='+videoUuid)
-        if url is not None :
-            addLink('[COLOR red]'+timeDisplay+'[/COLOR][B][COLOR blue]'+title+'[/COLOR][/B]', url, 20, image,image)
+def vi_read(url,page=None,channel=None):
+    try:
+        urldir=url
+        livejson = GetHTML('http://apiv2.voetbalinside.livewallcampaigns.com/'+str(channel)+'?page='+str(page)+url)
+        livejson = json.loads(livejson, encoding='utf-8')
+        for items in livejson:
+            name = str(items)
+            for items in livejson[name]["items"]:
+                title = items["title"]
+                title = title.encode('utf-8')
+                title = removeNonAscii(title)
+                timeDisplay = items["timeDisplay"]
+                timeDisplay =    timeDisplay.rstrip(timeDisplay[-6:])
+                if "videoUuid" in items:
+                    videoUuid = items["videoUuid"]
+                    image = items["image"]
+                    image = 'http://rtl.lwcdn.nl/imageScaled/?site=voetbalinside&file='+image+'&w=500.000000&h=500.000000&cropped=1'
+                    url = get_video('http://www.rtl.nl/system/s4m/vfd/version=2/d=iphone/fmt=adaptive/fun=abstract/uuid='+videoUuid)
+                    if url is not None :
+                        addLink('[B][COLOR blue]'+title+ '[/COLOR][/B][COLOR red] ('+timeDisplay+')[/COLOR]', url, 20, image,image)
+    except:
+        pass
+    if not "home" in channel:
+        try:
+            page =page+1
+            livejson = GetHTML('http://apiv2.voetbalinside.livewallcampaigns.com/'+str(channel)+'?page='+str(page)+urldir)
+            if not '"items":false' in livejson:   
+                addDir('Previous Page' ,urldir,1,icon,page,channel,fanart=fanart)
+        except:
+            pass
+            
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+
+
+def search():
+    keyboard = xbmc.Keyboard('', 'Enter Zoek Naam:', False)
+    keyboard.doModal()
+    if keyboard.isConfirmed():
+        query = keyboard.getText()
+    else:
+        return
+    query = query.encode('utf-8')
+    query = removeNonAscii(query)
+    vi_read('&searchTerm='+query,page=1,channel='search')
+    
 
 
 def get_video(url):
@@ -164,34 +194,30 @@ def addLink(name,url,mode,iconimage,fanartimage):
     return ok
 
 
-def addDir(name,url,mode,iconimage):
-    u = (sys.argv[0] +
-         "?url=" + urllib.quote_plus(url) +
-         "&mode=" + str(mode) +
-         "&name=" + urllib.quote_plus(name))
+def addDir(name, url, mode, iconimage, page,channel, section=None, keyword='', Folder=True, fanart=None):
+    if url.startswith("plugin://"):
+        u = url
+    else:
+        u = (sys.argv[0] +
+             "?url=" + urllib.quote_plus(url) +
+             "&mode=" + str(mode) +
+             "&page=" + str(page) +
+             "&channel=" + str(channel) +
+             "&section=" + str(section) +
+             "&keyword=" + urllib.quote_plus(keyword) +
+             "&name=" + urllib.quote_plus(name))
     ok = True
     liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-    liz.setInfo(type="Video", infoLabels={ "Title": name })
-    liz.setProperty("Fanart_Image", fanart)
-    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
+    liz.setArt({'thumb': iconimage, 'icon': iconimage})
+    if fanart:
+        liz.setArt({'fanart': fanart})
+    liz.setInfo(type="Video", infoLabels={"Title": name})
+    ok = xbmcplugin.addDirectoryItem(handle=addon_handle, url=u, listitem=liz, isFolder=Folder)
     return ok
 
 
 
-def search():
-    keyboard = xbmc.Keyboard('', 'Enter Zoek Naam:', False)
-    keyboard.doModal()
-    if keyboard.isConfirmed():
-        query = keyboard.getText()
-    else:
-        return
-    query = query.encode('utf-8')
-    query = removeNonAscii(query)
-    url = ziggosearch+query
-    try:
-        ziggoread(url)
-    except:
-        return
+
     
 
 
@@ -199,7 +225,10 @@ params = getParams()
 url = None
 name = None
 mode = None
+page = None
 download = None
+channel = None
+section=None
 
 try: url = urllib.unquote_plus(params["url"])
 except: pass
@@ -207,11 +236,16 @@ try: name = urllib.unquote_plus(params["name"])
 except: pass
 try: mode = int(params["mode"])
 except: pass
-
+try: page=int(params["page"])
+except: pass
+try: channel=str(params["channel"])
+except: pass
+try: section=str(params["section"])
+except: pass
 
 
 if mode == None: MainDir()
-elif mode == 1: vi_read(url)
+elif mode == 1: vi_read(url,page,channel)
 elif mode == 2: search()
 
 
